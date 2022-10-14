32f3514be2f2673d932db4c1ebafadf5663d9e17
pino._.hoo <inhoo23@naver.com>
2022-10-15 00:28:48 +0900

    add : alert screen

--- pages/root/AlertScreen.tsx
+++ pages/root/AlertScreen.tsx
@@ -1,9 +1,44 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
-export default function AlertScreen() {
+export default function AlertScreen({navigation}: any) {
+  const onPressHome = () => {
+    navigation.navigate('Bottom');
+  };
+
   return (
-    <View>
-      <Text>알림창</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity style={styles.headerBtn} onPress={onPressHome}>
+          <Text>뒤로가기</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.bodyText}>메세지가 없습니다.</Text>
+      </View>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 10,
+    flexDirection: 'row',
+  },
+  headerBtn: {
+    marginLeft: 30,
+  },
+  body: {
+    flex: 7,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  bodyText: {
+    fontSize: 15,
+    marginTop: -50,
+    color: 'grey',
+  },
+});
